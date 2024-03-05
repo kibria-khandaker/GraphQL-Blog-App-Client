ba17aba93261ed31e3f57c36278cd79c7989ae69
kibria-khandaker <kibriakhandaker@gmail.com>
2024-03-06 04:09:46 +0600

    Reg & login option with set token in local store Done

--- src/pages/Signin/Signin.js
+++ src/pages/Signin/Signin.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import { gql, useMutation } from '@apollo/client';
+import React, { useEffect, useState } from 'react';
+
+const LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        signin(email: $email, password: $password) {
+            userError
+            token
+        }
+    }
+`
 
 const Signin = () => {
+
+    const [login, { data, error, loading }] = useMutation(LOGIN);
+
     const handleRegister = (e) => {
         e.preventDefault();
         const data = {
             email: e.target.email.value,
             password: e.target.password.value,
         }
-        console.log("data: ", data)
+
+        login({
+            variables: data
+        })
     }
+
+    console.log("data: ", data)
+    const [userError, setUserError] = useState(null);
+
+    useEffect(() => {
+        if (data && data?.signin?.token) {
+            console.log("token", data.signin.token)
+            localStorage.setItem("token", data.signin.token)
+        }
+        if (data && data.signin?.userError) {
+            setUserError(data.signin.userError)
+        }
+    }, [data]);
 
     return (
         <div className="form">
@@ -19,7 +48,7 @@ const Signin = () => {
                 <label htmlFor="">Your Password</label>
                 <input name="password" type="password" />
 
-                <button className='rounded-full p-2 bg-white text-black'>Login</button>
+                <button type="submit" className='rounded-full p-2 bg-white text-black'>Login</button>
             </form>
         </div>
     );
