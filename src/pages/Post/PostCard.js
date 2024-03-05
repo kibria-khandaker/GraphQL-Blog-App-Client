9f42529cba1e18ed32ce2cec9bced3281cdccd17
kibria-khandaker <kibriakhandaker@gmail.com>
2024-03-06 03:21:34 +0600

    JS Date formatted to normal Date

--- src/pages/Post/PostCard.js
+++ src/pages/Post/PostCard.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './PostCard.css'
 
 const PostCard = ({post}) => {
-    console.log(post);
+    // console.log(post);
+    // JS Date formatted to normal Date start ------------------------
+    const [formattedDate, setFormattedDate] = useState("")
+    useEffect(()=>{
+        if(post.createdAt){
+            let date = post.createdAt;
+            date /= 1000;
+            const dateInstance = new Date(date);
+            // console.log(dateInstance.getFullYear());
+            setFormattedDate(`${dateInstance.getDate()}-${dateInstance.getMonth()+1}-${dateInstance.getFullYear()}`)
+        }
+    },[post])
+    // console.log(formattedDate);
+    // JS Date formatted to normal Date End ------------------------
     return (
         <div className='border w-96 m-4 p-4 rounded-md border-black-400'>
             <h3 className='text-lg font-bold'>This is post Title</h3>
             <div className='text-state-500 text-sm text-gray-700 flex justify-between py-2'>
                 <p>Author Name: {post.author.name}</p>
-                <p>Date: {post.createdAt}</p>
+                {/* <p>Date: {post.createdAt}</p> */}
+                <p>Date: {formattedDate}</p>
             </div>
             <p className='text-sm'>This is Content:  {post.author.content}</p>
         </div>
--- src/pages/Post/Posts.js
+++ src/pages/Post/Posts.js
@@ -21,15 +21,17 @@ const Posts = () => {
     // console.log(data);
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
-    console.log(data.posts);
+    // console.log(data.posts);
 
     return (
         <div>
             <h1 className='text-center font-bold text-5xl my-4 pb-4'>Posts</h1>
             <hr />
+            <div className=' flex float-wrap'>
             {
                 data.posts.map((post, index) => <PostCard key={index} post={post}></PostCard>)
             }
+            </div>
         </div>
     );
 };
