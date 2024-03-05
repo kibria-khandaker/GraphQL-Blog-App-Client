e6ab068369312ecc2884e36f5eba5c72f6e2d688
kibria-khandaker <kibriakhandaker@gmail.com>
2024-03-06 03:05:29 +0600

    Data loaded success from Backend  using  GraphQL  and PostgreSQL

--- src/pages/Post/PostCard.js
+++ src/pages/Post/PostCard.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './PostCard.css'
 
-const PostCard = () => {
+const PostCard = ({post}) => {
+    console.log(post);
     return (
         <div className='border w-96 m-4 p-4 rounded-md border-black-400'>
             <h3 className='text-lg font-bold'>This is post Title</h3>
             <div className='text-state-500 text-sm text-gray-700 flex justify-between py-2'>
-                <p>Author Name</p>
-                <p>Date: </p>
+                <p>Author Name: {post.author.name}</p>
+                <p>Date: {post.createdAt}</p>
             </div>
-            <p className='text-sm'>This is Content</p>
+            <p className='text-sm'>This is Content:  {post.author.content}</p>
         </div>
     );
 };
--- src/pages/Post/Posts.js
+++ src/pages/Post/Posts.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import PostCard from './PostCard';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_POSTS = gql`
+    query PostData{
+        posts {
+        title
+        content
+        createdAt
+        author {
+            name
+            email
+        }
+        }
+    }
+`;
 
 const Posts = () => {
+    const { loading, error, data } = useQuery(GET_POSTS)
+    // console.log(data);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+    console.log(data.posts);
+
     return (
         <div>
             <h1 className='text-center font-bold text-5xl my-4 pb-4'>Posts</h1>
             <hr />
-            <PostCard></PostCard>
+            {
+                data.posts.map((post, index) => <PostCard key={index} post={post}></PostCard>)
+            }
         </div>
     );
 };
